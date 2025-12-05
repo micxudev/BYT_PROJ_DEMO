78207c3dbe9a6a50081a88f2971086497f20e945
dredd <1misha.baltovsky@gmail.com>
2025-12-05 21:05:11 +0100

    Add `JsonSerializerExtensions`

---
+++ Json/JsonSerializerExtensions.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace _PRO.Json;
+
+public static class JsonSerializerExtensions
+{
+    private static readonly JsonSerializerSettings Options = new()
+    {
+        Formatting = Formatting.Indented,
+        TypeNameHandling = TypeNameHandling.Auto,
+        PreserveReferencesHandling = PreserveReferencesHandling.All,
+        ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+        // ContractResolver = new ParentFirstContractResolver() // not needed for demo
+    };
+
+    static JsonSerializerExtensions()
+    {
+        Options.Converters.Add(new StringEnumConverter());
+    }
+
+    public static string ToJson(this IEnumerable collection)
+    {
+        return JsonConvert.SerializeObject(collection, Options);
+    }
+}
