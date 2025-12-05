7be2d1f50d6d3fdcf876fef7b7e0a436448678d6
dredd <1misha.baltovsky@gmail.com>
2025-12-05 21:34:00 +0100

    Load `Product` and `Order` data from JSON files, add default data initialization, and adjust output and save logic accordingly.

--- Program.cs
+++ Program.cs
@@ -1,34 +1,50 @@
 ﻿using _PRO.Json;
 using _PRO.Models;
 
-// ----------< Try Load From File >----------
-var customersFile = new FileInfo("./db/customers.json");
-var loaded = JsonUtils.Load(customersFile, new List<Customer>());
-Console.WriteLine($"Loaded {loaded.Count} customers from file {customersFile.FullName}");
+// ----------< Try Load From Files >----------
+var productsFile = new FileInfo("./db/products.json");
+var loadedProducts = JsonUtils.Load(productsFile, new List<Product>());
+Console.WriteLine($"Loaded {loadedProducts.Count} products from file {productsFile.FullName}");
+
+var ordersFile = new FileInfo("./db/orders.json");
+var loadedOrders = JsonUtils.Load(ordersFile, new List<Order>());
+Console.WriteLine($"Loaded {loadedOrders.Count} orders from file {ordersFile.FullName}");
 
 // -----< Add Some Data If Nothing Loaded >-----
-if (loaded.Count == 0)
+if (loadedProducts.Count == 0 && loadedOrders.Count == 0)
 {
     try
     {
-        new Customer(
-            1,
-            "John",
-            "Doe",
-            "+123456789",
-            "john.doe@gmail.com",
-            "123456",
-            DateTime.Now
+        // -----< Products >-----
+        var p1 = new Product(
+            "Product 1",
+            100m
+        );
+        var p2 = new Product(
+            "Product 2",
+            200m
+        );
+        var p3 = new Product(
+            "Product 3",
+            300m
         );
 
-        new Customer(
-            2,
-            "Jane",
-            "Doe",
-            "+123456789",
-            "jane.doe@gmail.com",
-            "123456",
-            DateTime.Now.Subtract(TimeSpan.FromDays(365 * 3))
+        // -----< Orders >-----
+        var o1 = new Order(
+            DateTime.Now,
+            new Dictionary<Product, int>
+            {
+                { p1, 2 },
+                { p2, 3 }
+            }
+        );
+        var o2 = new Order(
+            DateTime.Now,
+            new Dictionary<Product, int>
+            {
+                { p2, 4 },
+                { p3, 6 }
+            }
         );
     }
     catch (Exception ex)
@@ -40,12 +56,19 @@ if (loaded.Count == 0)
 
 
 // ----------< Output >----------
-Console.WriteLine("\nCustomer.All:");
-foreach (var customer in Customer.All)
-    Console.WriteLine(customer);
+Console.WriteLine("Product.All:");
+foreach (var product in Product.All)
+    Console.WriteLine(product);
+
+Console.WriteLine("\n----------------------------------------\n");
+
+Console.WriteLine("Order.All:");
+foreach (var order in Order.All)
+    Console.WriteLine(order);
 
 
-// ----------< Save To File >----------
-JsonUtils.SaveAsync(Customer.All, customersFile);
+// ----------< Save To Files >----------
+JsonUtils.SaveAsync(Product.All, productsFile);
+JsonUtils.SaveAsync(Order.All, ordersFile);
 
 await JsonUtils.ShutdownSaveExecutorAsync();
